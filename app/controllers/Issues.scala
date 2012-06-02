8d1067983751ab7ad7760ac188eaf4ae3698c51d
jcdelmas <jc.delmas@gmail.com>
2012-06-02 13:27:28 +0200

    List sample issues in a table

--- app/controllers/Issues.scala
+++ app/controllers/Issues.scala
@@ -1,8 +1,15 @@
 package controllers
 
 import play.api.mvc._
+import models._
+import views._
 
 object Issues extends Controller {
 
-  def list = TODO
+  def list = Action {
+    Ok(html.listIssues(List(
+        Issue(100, "Bug1", "Lorem Ipsum"),
+        Issue(101, "Bug2", "Lorem Ipsum")
+    )))
+  }
 }
---
+++ app/models/Issue.scala
@@ -0,0 +1,7 @@
+package models
+
+case class Issue (
+    number: Long,
+    title: String,
+    description: String
+)
