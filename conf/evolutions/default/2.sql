707dfb6715587c214fd27b19beb85b715e02701b
jcdelmas <jc.delmas@gmail.com>
2012-06-12 00:49:16 +0200

    Add new Test User in evolution file

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -5,7 +5,7 @@
 set ignorecase true;
 
 insert into user_ values (user_seq.nextval, 'jcdelmas', 'jc.delmas@gmail.com', 'password');
- 
+insert into user_ values (user_seq.nextval, 'Test User', 'test.user@yopmail.com', 'password');
 # --- !Downs
 
 delete from user_; 
